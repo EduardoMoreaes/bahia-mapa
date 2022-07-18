2ff9dd383f3484455667305187d4acab26faa941
Eduardo Moraes <88555769+EduardoMoreaes@users.noreply.github.com>
2022-07-17 21:54:48 -0300

    Update bahia.R

--- bahia.R
+++ bahia.R
@@ -1,9 +1,9 @@
-install.packages ("rgdal")
-install.packages ("ggplot2")
-install.packages("rgeos")
-install.packages("gpclib")
-install.packages("maptools")
-install.packages ("readr")
+#install.packages ("rgdal")
+#install.packages ("ggplot2")
+#install.packages("rgeos")
+#install.packages("gpclib")
+#install.packages("maptools")
+#install.packages ("readr")
 
 library(gpclib)
 library(maptools)
@@ -12,9 +12,10 @@ library (ggplot2)
 library (rgdal)
 library (readr)
 
-bh <- readOGR("C:/", "29MUE250GC_SIR")
+bh <- readOGR(file.choose())
+head(bh)
 
-bh$CD_GEOCODM  <- substr(bh$CD_GEOCODM,1,6)   
+bh$CD_GEOCODM  <- substr(bh$CD_GEOCODM,1,6) 
 nascimentos <- read.csv2(file.choose(), header = T, sep = ",")     #o header é f, porque o csv não twm cabeçalho
 
 
@@ -24,6 +25,7 @@ nascimentos$CD_GEOCODM <- substr(nascimentos$municipio,1,6)
 
 nascimentos <- nascimentos[order(nascimentos$CD_GEOCODM),] 
 malhaBH <- bh@data[order(bh@data$CD_GEOCODM),]
+nascimentos <- nascimentos[-1,]
 head(malhaBH)
 head(nascimentos)
 
@@ -36,12 +38,14 @@ head(bh)
 
 ###################################
 
-library (ggplot2)
-bh.bhf <- fortify (bh, region = "CD_GEOCODM")
 
+bh.bhf <- fortify(bh, region = "CD_GEOCODM")
+head(bh.bhf)
 
-head (bh)
 bh.bhf <- merge(bh.bhf, bh@data, by.x = "id", by.y = "CD_GEOCODM")
+bh.bhf <- merge(bh.bhf, bh2, by.x = "id", by.y = "CD_GEOCODM")
+head(bh.bhf)
+bh.bhf <- bh.bhf[,-c(8,10,11, 12)]
 ############até aqui ok
 
 bh.bhf$nascimentosCat <- cut(bh.bhf$nascimentos, breaks = c(0,50,100,200,300,400,1000),
@@ -53,18 +57,14 @@ bh.bhf$nascimentosCat <- cut(bh.bhf$nascimentos, breaks = c(0,50,100,200,300,400
                                    '+1000'),
                         include.lowest = T)
 
-head(bh2)
-
-
-bh.bhf <- merge(bh.bhf, bh2, by.x = "id", by.y = "CD_GEOCODM")
-
 
 install.packages("RColorBrewer",dependencies = T)
 library(RColorBrewer)
 
-mapa_ggplot <-ggplot(bh.bhf, aes(long, lat, group=group, fill= nascimentos))+
+mapa_ggplot <-ggplot(bh.bhf, aes(long, lat, group=group, fill= nascimentosCat))+
   geom_polygon(colour='black')+ coord_equal() + ggtitle("Nascimentos na Bahia em 2020") +
   labs(x = "Longitude", y = "Latitude", fill="nascimentos") + 
+  scale_fill_manual(values=brewer.pal(9, 'PuRd')[4:9])+
   theme(plot.title=element_text(hjust = 0.5) );
 
   mapa_ggplot  
